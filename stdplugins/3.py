5a13fe963f91520d57ad62526d71f596606ffbdc
amansharma999 <aman.gamestar999@gmail.com>
2020-08-02 22:03:29 +0530

    Uploaded New Plugin

---
+++ stdplugins/3.py
@@ -0,0 +1,36 @@
+"""Fun pligon...for HardcoreUserbot
+\nCode by @Rambo_86
+type `.3` to see the fun.
+"""
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="3 ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("teri ammy ka boxda ka bujiya bana dunga")
+        await asyncio.sleep(0.7)
+        await event.edit("gandu rand ki paidais jhantu sale")
+        await asyncio.sleep(1)
+        await event.edit("loda chusega meraa maderchod sale tatti")
+        await asyncio.sleep(0.8)
+        await event.edit("teri ammy ke muh pe mut ke bhag jaunga maderchod")
+        await asyncio.sleep(0.9)
+        await event.edit("tu mera beta hai mai tera baap hun teri ammy roz chudwati hai mujhse")
+        await asyncio.sleep(1)
+        await event.edit("baap se batmizi krega behnchod")
+        await asyncio.sleep(0.8)
+        await event.edit("chuchi bada ke dudh pee jaunga teri ammy ki")
+        await asyncio.sleep(0.7)
+        await event.edit("thuk laga ke pelunga sale maderchod bakchodi kiya toh")
+        await asyncio.sleep(1)
+        await event.edit("`tere ammy ke gand mei morpankhi laga dunga`")
+        await asyncio.sleep(0.7)
+        await event.edit("`muh pakad ke pattar se lada lada ke maar dalunga teri ammy ko`")
+        await asyncio.sleep(0.8)
+        await event.edit("teri daaadi k chut me mombati jala dunga behmcho `")
+        await asyncio.sleep(1)
+        await event.edit(" s         s `")
+        
