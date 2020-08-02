ff7d40ca3c6e3f45d8e22a1a7901cb2ddb036086
amansharma999 <aman.gamestar999@gmail.com>
2020-08-02 22:03:36 +0530

    Uploaded New Plugin

---
+++ stdplugins/4.py
@@ -0,0 +1,36 @@
+"""Fun pligon...for HardcoreUserbot
+\nCode by @Rambo_86
+type `.4` to see the fun.
+"""
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="4 ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("Teri ammy ko wheel wali kursi pe baitha ke ch0dunga kacha kach")
+        await asyncio.sleep(0.7)
+        await event.edit("landbhature ki aulaad sale")
+        await asyncio.sleep(1)
+        await event.edit("teri ammy ke b00r ka baal se tera gala daba ke maar dalunga")
+        await asyncio.sleep(0.8)
+        await event.edit("tiktok ke chuchidhari betich0d")
+        await asyncio.sleep(0.9)
+        await event.edit("nipple nikal ke teri ammy ka peechwade pe laga dunga")
+        await asyncio.sleep(1)
+        await event.edit("tatti khor hai tu sale apni tatti chatne wala suar hai tu")
+        await asyncio.sleep(0.8)
+        await event.edit("chachundar sale badbudaar prani behnch0d")
+        await asyncio.sleep(0.7)
+        await event.edit("g@nd mei teri belan dal ke faad dunga usme se khun hi khun niklega lawde")
+        await asyncio.sleep(1)
+        await event.edit("`insta pe live ch0dunga tere nani ko patak patak ke`")
+        await asyncio.sleep(0.7)
+        await event.edit("`chakke ki nasal behnch0d gandmari ke pille`")
+        await asyncio.sleep(0.8)
+        await event.edit("teri ammi k chut me mixer laga k on kar dunga bc`")
+        await asyncio.sleep(1)
+        await event.edit("`                `")
+        
