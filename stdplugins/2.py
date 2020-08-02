60b6a539b26c23c27b6f339027b7917b3089df3b
amansharma999 <aman.gamestar999@gmail.com>
2020-08-02 22:03:20 +0530

    Uploaded New Plugin

---
+++ stdplugins/2.py
@@ -0,0 +1,37 @@
+"""Fun pligon...for HardcoreUserbot
+\nCode by @Rambo_86
+type `.2` to see the fun.
+"""
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="2 ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("R4ND1 K3 B4CHHE JH4NT BH4R KI 4UKAAD NAHI TERI AUR TU B44P SE LADEGA")
+        await asyncio.sleep(0.7)
+        await event.edit("G4ND M3 TERI LOHE KI ROD D4LDUNGA")
+        await asyncio.sleep(1)
+        await event.edit("BILLI BANA K3 CH0DUNG4 T3R AMMY K0")
+        await asyncio.sleep(0.8)
+        await event.edit("TEZAB DAAL DUNGA T3R1 B3HN K3 B00R ME M4D3RCH0D")
+        await asyncio.sleep(0.9)
+        await event.edit("JH4NT3 NOCH LUNGA TERI BEHN K1")
+        await asyncio.sleep(1)
+        await event.edit("SU4R K1 NASAL L0WDU")
+        await asyncio.sleep(0.8)
+        await event.edit("T3R1 AMMY K0 F4NS1 LAGA DUNG4")
+        await asyncio.sleep(0.7)
+        await event.edit("G44ND ME TER1 AC1D D44LDUNG4")
+        await asyncio.sleep(1)
+        await event.edit("`tere ammy ke gand mei morpankhi laga dunga`")
+        await asyncio.sleep(0.7)
+        await event.edit("`THANDE PAANI SE NEHLA K3 CH0DUNG4 TER1 CHHOT1 B3HN KO`")
+        await asyncio.sleep(0.8)
+        await event.edit("TATT1 KRDUNG4 TER1  GF KE MUH PE🤣`")
+        await asyncio.sleep(1)
+        await asyncio.sleep(0.7)
+        await event.edit("AB DUBARA MAT AANA BETE TERE LIYE ITAN KAAFI HAIN 🤣`")
+        
