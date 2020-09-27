dc3e833b0c651b1a97d4289ff377d1033f3c1765
amansharma999 <aman.gamestar999@gmail.com>
2020-09-28 00:32:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,11 +67,11 @@ class Config(Config):
     # maximum number of messages for antiflood
     MAX_ANTI_FLOOD_MESSAGES = 10
     # warn mode for anti flood
-    ANTI_FLOOD_WARN_MODE = ChatBannedRights(
-        until_date=None,
-        view_messages=None,
-        send_messages=True
-    )
+    #ANTI_FLOOD_WARN_MODE = ChatBannedRights(
+        #until_date=None,
+        #view_messages=None,
+        #send_messages=True
+    #)
     # chat ids or usernames, it is recommended to use chat ids,
     # providing usernames means an additional overhead for the user
    # CHATS_TO_MONITOR_FOR_ANTI_FLOOD = []
